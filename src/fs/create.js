ae7d427682710dd0456ef143d0bd32bb007639b6
rina4790 <rina4790karina@gmail.com>
2022-11-30 16:08:08 +0100

    feat: create.js - implement function that creates new file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
+import * as fs from "node:fs";
+
 const create = async () => {
-    // Write your code here 
+  fs.readFile("src/fs/files/fresh.txt", "utf8", function (err) {
+    if (err) {
+      fs.writeFile(
+        "src/fs/files/fresh.txt",
+        "I am fresh and young",
+        function (err) {
+          if (err) console.log("error", err);
+        }
+      );
+    } else {
+      const err = new Error("FS operation failed");
+      console.log(err);
+    }
+  });
 };
 
 await create();
