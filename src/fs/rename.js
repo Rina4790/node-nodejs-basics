a674378b331cb056fcb8033ccb077d465fd19de1
rina4790 <rina4790karina@gmail.com>
2022-12-01 10:58:33 +0100

    feat: rename.js - implement function that renames file

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,25 @@
-const rename = async () => {
-    // Write your code here 
+import * as fs from "node:fs";
+import { rename } from "node:fs/promises";
+
+const rename1 = async () => {
+  const renameFile = async () => {
+    try {
+      await rename(
+        "src/fs/files/wrongFilename.txt",
+        "src/fs/files/properFilename.md"
+      );
+    } catch {
+      throw new Error("FS operation failed");
+    }
+  };
+
+  fs.open("src/fs/files/properFilename.md", (err) => {
+    if (err) {
+      renameFile();
+    } else {
+      throw new Error("FS operation failed");
+    }
+  });
 };
 
-await rename();
+await rename1();
