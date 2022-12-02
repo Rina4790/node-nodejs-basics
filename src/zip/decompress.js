5cb1ab578592c1d25edcc847e9d009bc20df68ac
rina4790 <rina4790karina@gmail.com>
2022-12-02 19:50:46 +0100

    feat: Zlib (compress and decompress)

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,19 @@
+import * as fs from "node:fs";
+import * as zlib from "node:zlib";
+import { pipeline } from "node:stream";
+
 const decompress = async () => {
-    // Write your code here 
+  const archive = fs.createReadStream("src/zip/files/archive.gz");
+  const fileToDecompress = fs.createWriteStream(
+    "src/zip/files/fileToCompress.txt"
+  );
+  const gunzip = zlib.Gunzip();
+
+  pipeline(archive, gunzip, fileToDecompress, (err) => {
+    if (err) {
+      console.error("Error", err.message);
+    }
+  });
 };
 
 await decompress();
